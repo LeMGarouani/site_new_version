c28ee6adb02d656b4c365b865f7c68c35fa407f7
LeMGarouani <80177661+LeMGarouani@users.noreply.github.com>
2026-02-25 16:56:26 +0100

    +

---
+++ static/teaching/2020-2021/M1/Agility/TP1/ApiCall.java
@@ -0,0 +1,46 @@
+package services;
+
+import com.google.gson.Gson;
+import spark.utils.IOUtils;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.fail;
+
+public class ApiCall {
+    public static TestResponse request(String method, String path, String requestBody) {
+
+        try {
+            URL url = new URL("http://localhost:4567" + path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(method);
+            connection.setDoOutput(true);
+            connection.connect();
+            String body = IOUtils.toString(connection.getInputStream());
+            return new TestResponse(connection.getResponseCode(), body);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Sending request failed: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static class TestResponse {
+
+        public final String body;
+        public final int status;
+
+        public TestResponse(int status, String body) {
+            this.status = status;
+            this.body = body;
+        }
+
+        public Map<String,String> json() {
+            return new Gson().fromJson(body, HashMap.class);
+        }
+    }
+}
